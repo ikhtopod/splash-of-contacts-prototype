6fc3c6d1c68b8487241a9a5e2ec4e213649a4059
insoLLLent <vitaly@lifanoff.ru>
2019-09-09 13:49:46 +0300

    updated

--- Demo/Demo.gd
+++ Demo/Demo.gd
@@ -9,3 +9,7 @@ func _process(delta):
 		var position_node = get_node("Control/PanelContainer/HBoxContainer/position/" + str(i))
 		pressed_node.text = str(TouchManager.current_touch.At(i).IsPressed())
 		position_node.text = str(TouchManager.current_touch.At(i).GetPosition())
+
+
+func _on_TouchScreenButton_pressed():
+	get_tree().quit()
