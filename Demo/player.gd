e0cf0305102ae1429bf08de8812f71df794e6413
insoLLLent <vitaly@lifanoff.ru>
2019-09-09 12:24:14 +0300

    updated scripts

--- Demo/player.gd
+++ Demo/player.gd
@@ -1,9 +1,4 @@
 extends KinematicBody2D
 
-onready var dragging = false
-
 func _input(event):
-	if (event is InputEventScreenTouch): # need detect collision
-		dragging = event.pressed
-	elif (event is InputEventScreenDrag) and dragging:
-		position = event.position
+    pass
